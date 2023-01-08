956ebe9e951af57ca0f1dd00013772d241d6c7d6
Thierry Philippe <thierry.philippe@external.engie.com>
2023-01-08 21:38:35 +0100

    Improved some styling and added toggle detail button on mobile

--- src/main.js
+++ src/main.js
@@ -22,25 +22,42 @@ tableWrapperNode.innerHTML = tableify(prepareObj(vocabulary));
 
 new Tablesort(document.querySelector("table"));
 
+// const optionsQueryParams = "&dmsm=0&wdrst=1&rfs=1&dhlm=0";
+const optionsQueryParams = "";
+
 const detailsNode = document.querySelector("[data-details]");
 const detailsIframeNode = document.querySelector("[data-details-iframe]");
 const trNodes = [...tableWrapperNode.querySelectorAll("tbody tr")];
-detailsIframeNode.src = `https://www.mdbg.net/chinese/dictionary?page=worddict&wdrst=1&wdqb=${
+detailsIframeNode.src = `https://www.mdbg.net/chinese/dictionary?page=worddict${optionsQueryParams}&wdqb=${
 	trNodes[0].querySelector("[data-details-open]").textContent
 }`;
 
+let isOpened = false;
+
 document.addEventListener("click", (ev) => {
 	const openNode = ev.target.closest("[data-details-open]");
-	const closeNode = ev.target.closest("[data-details-close]");
+	const toggleNode = ev.target.closest("[data-details-toggle]");
 
 	if (openNode) {
-		detailsNode.hidden = false;
-		detailsIframeNode.src = `https://www.mdbg.net/chinese/dictionary?page=worddict&wdrst=1&wdqb=${openNode.dataset.detailsOpen}`;
+		detailsIframeNode.style.opacity = 0;
+		detailsIframeNode.src = `https://www.mdbg.net/chinese/dictionary?page=worddict${optionsQueryParams}&wdqb=${openNode.dataset.detailsOpen}`;
+		detailsIframeNode.addEventListener(
+			"load",
+			() => (detailsIframeNode.style.opacity = 1)
+		);
+		if (!isOpened) {
+			isOpened = true;
+			detailsNode.style.transform = "translateY(0)";
+		}
 	}
 
-	if (closeNode) {
-		detailsNode.hidden = true;
-		detailsIframeNode.src = "";
+	if (toggleNode) {
+		if (isOpened) {
+			detailsNode.style.transform = "translateY(100%)";
+		} else {
+			detailsNode.style.transform = "translateY(0)";
+		}
+		isOpened = !isOpened;
 	}
 });
 
