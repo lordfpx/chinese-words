2253a43811afcf3c24ab1a5d5872eccc2a77190b
Thierry Philippe <thierry.philippe@external.engie.com>
2023-12-18 23:11:41 +0100

    Added lesson 2 and vocabulary

--- src/data/texts-2.js
+++ src/data/texts-2.js
@@ -35,4 +35,40 @@ Yes, I can't go out for a run when it rains.
 The weather forecast says it may rain tomorrow.
 I hope it will be sunny tomorrow.`,
 	},
+	{
+		lesson: "2-2",
+		chinese: `大哥，我下個星期回臺灣。這個星期六我想去給爸爸媽媽買禮物。
+你想給他們買什麼？
+我想給爸爸買一個帽子，給媽媽買一件衣服，還想買兩盒巧克力。
+你要不要上網買？在網路商店買東西比較方便。
+網路商店的東西又好又便宜。可是因為我喜歡逛街，所以想去百貨公司買。
+好，這個週末我有空，我們去百貨公司買。
+我也想給姐姐買一個背包。
+對，她喜歡旅行,背包很有用。還可以給她買太陽眼鏡。
+星期六我們什麼時候出門？
+下午一點，好不好？
+好，謝謝大哥。`,
+		pinyin: `Dàgē, wǒ xià gè xīngqí huí táiwān. Zhège xīngqíliù wǒ xiǎng qù gěi bàba māmā mǎi lǐwù.
+Nǐ xiǎng gěi tāmen mǎi shénme?
+Wǒ xiǎng gěi bàba mǎi yīgè màozi, gěi māmā mǎi yī jiàn yīfú, hái xiǎng mǎi liǎng hé qiǎokèlì.
+Nǐ yào bùyào shàngwǎng mǎi? Zài wǎng lù shāngdiàn mǎi dōngxī bǐjiào fāngbiàn.
+Wǎng lù shāngdiàn de dōngxī yòu hǎo yòu piányí. Kěshì yīnwèi wǒ xǐhuān guàngjiē, suǒyǐ xiǎng qù bǎihuò gōngsī mǎi.
+Hǎo, zhège zhōumò wǒ yǒu kòng, wǒmen qù bǎihuò gōngsī mǎi.
+Wǒ yě xiǎng gěi jiějiě mǎi yīgè bèibāo.
+Duì, tā xǐhuān lǚxíng, bèibāo hěn yǒuyòng. Hái kěyǐ gěi tā mǎi tàiyáng yǎnjìng.
+Xīngqíliù wǒmen shénme shíhòu chūmén?
+Xiàwǔ yīdiǎn, hǎobù hǎo?
+Hǎo, xièxiè dàgē.`,
+		english: `Brother, I will go back to Taiwan next week. This Saturday I want to buy gifts for my parents.
+What do you want to buy them?
+I want to buy a hat for my dad, a dress for my mom, and two boxes of chocolates.
+Do you want to buy online? It is more convenient to buy things in online stores.
+The stuff in online stores is good and cheap. But because I like shopping, I want to go to a department store to buy it.
+Okay, I'm free this weekend, let's go to the department store to buy some.
+I also want to buy a backpack for my sister.
+Yes, she likes to travel and the backpack is very useful. You can also buy her sunglasses.
+When do we go out on Saturday?
+One o'clock in the afternoon, okay?
+OK, thank you, brother.`,
+	},
 ];
